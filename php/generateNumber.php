3b2747d66027c19caded8a478dc08f35638a9b25
Subigya Pokharel <subigyapok08@gmail.com>
2026-04-27 21:35:50 +0545

    starting to document my shit

--- php/generateNumber.php
+++ php/generateNumber.php
@@ -1,13 +1,14 @@
 <?php
 
-// generate secret number only once
-if (empty($_SESSION['secret'])) {
+if (empty($_SESSION['secret'])) { // generate secret number only once
+
     $digits = "123456789";
-    $first = $digits[rand(0, 8)];
+    $first = $digits[rand(0, 8)]; // selects one random digit
 
-    $remaining = str_replace($first, "", "0123456789");
-    $shuffled = str_shuffle($remaining);
+    $remaining = str_replace($first, "", "0123456789"); //removes the selected digit
+    $shuffled = str_shuffle($remaining); // shuffles the remaining
 
-    $_SESSION['secret'] = $first . substr($shuffled, 0, 3);
+    $_SESSION['secret'] = $first . substr($shuffled, 0, 3); // joins that random digit and 
+                                                            // 3 digits from shuffled
 }
 ?>
